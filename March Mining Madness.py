19aa53de1f7b205eb6a0a69f5c3058797a8b2029
Daniel Schepers <schepedw@rose-hulman.edu>
2014-02-07 01:14:25 -0500

    Can now summarize tournament wins/losses

--- March Mining Madness.py
+++ March Mining Madness.py
@@ -20,13 +20,28 @@ maxTeamNumber=teams.id.max()
 def main():
 	getWinLossTotals()
 
-def getWinLossTotals():#Could take a year as a param
-    winLosses=DataFrame(index=np.arange(minTeamNumber,maxTeamNumber+1),columns=['wins','losses'])
+def getWinLossTotals(season):#Pass 0 to get all season data
+    winLosses=DataFrame(index=np.arange(minTeamNumber,maxTeamNumber+1),columns=['wins','losses','winpct'])
+    regSeason=regular_season_results
+    if season!=0:
+        regSeason=regular_season_results.ix[regular_season_results.season==season]
     for i in np.arange(minTeamNumber,maxTeamNumber):
-        winLosses.loc[i][0]=regular_season_results.ix[regular_season_results.wteam==i].season.count()
-        winLosses.loc[i][1]=regular_season_results.ix[regular_season_results.lteam==i].season.count()
+        winLosses.loc[i][0]=regSeason.ix[regSeason.wteam==i].season.count()
+        winLosses.loc[i][1]=regSeason.ix[regSeason.lteam==i].season.count()
+    winLosses.winpct=winLosses.wins*1.0/(winLosses.wins+winLosses.losses)
     return winLosses
 
+def getTournamentWinLosses(season):#Needs to find whether a team was in the tournament in the given season
+    teams=tourney_results.wteam.unique()
+    winLosses=DataFrame(index=teams,columns=['wins','losses','winpct'])
+    tourney=tourney_results
+    if season!=0:
+        tourney=tourney_results.ix[regular_season_results.season==season]
+    for i in teams:
+        winLosses.loc[i][0]=tourney.ix[tourney.wteam==i].season.count()
+        winLosses.loc[i][1]=tourney.ix[tourney.lteam==i].season.count()
+    winLosses.winpct=winLosses.wins*1.0/(winLosses.wins+winLosses.losses)
+    return winLosses
 	
 	
 
